f645d308c3bc2770541e868840009df295c47ce3
Miraj Sutariya <=>
2024-05-28 04:47:52 +0530

    newcommit

---
+++ Secondcode.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+number=int(input('Enter Your Number:'))
+if number%2==0:
+    print('this number is even')
+else:
+    print('this number is odd')
+
+
+# In[ ]:
+
+
+
+
